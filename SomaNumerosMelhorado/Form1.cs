1464bd911d1152a52672178887c072a48e7ad7c3
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-04 22:06:12 -0300

    TryParse Implementation

--- SomaNumerosMelhorado/Form1.cs
+++ SomaNumerosMelhorado/Form1.cs
@@ -22,24 +22,35 @@ namespace SomaNumerosMelhorado
             int i, num, soma = 0;
 
             // extraindo número do TextBox
+            int number;
+            bool success = Int32.TryParse(txtNum.Text, out number);
+            if (success)
+            {
+                num = Convert.ToInt32(txtNum.Text);
 
-            num = Convert.ToInt32(txtNum.Text);
-
-            /* inicializa o laço em 1; *
+                /* inicializa o laço em 1; *
              * testa a condição de parada: i <= num *
              * incrementa a variavel i++ */
-            for (i = 1; i <= num; i++)
-            {
-                //verifica se i é par
-                if (i % 2 == 0)
+                for (i = 1; i <= num; i++)
                 {
-                    //faz o somatório dos números pares
-                    soma += i;
+                    //verifica se i é par
+                    if (i % 2 == 0)
+                    {
+                        //faz o somatório dos números pares
+                        soma += i;
+                    }
                 }
-            }
 
-            //apresenta o total da soma dos pares no txtSoma
-            txtSoma.Text = soma.ToString();
+                //apresenta o total da soma dos pares no txtSoma
+                txtSoma.Text = soma.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Digite um número!");
+            }
+                       
         }
+
     }
 }
